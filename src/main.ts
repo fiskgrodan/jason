aecc7f0c73e1b74cdf3811471a939732c9a05f4e
fiskgrodan <jam@Johans-MacBook-Pro.local>
2022-10-07 15:33:44 +0200

    refactor: use a mono font

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './app.css'
-import App from './App.svelte'
+import "@fontsource/roboto-mono/variable.css";
+import "./app.css";
+import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById('app')
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
